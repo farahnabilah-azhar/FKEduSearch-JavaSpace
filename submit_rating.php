ba9bba8771fe8874addabc214c98a908413735d2
ahmed <hulk9309@gmail.com>
2023-07-02 15:55:45 +0800

    small changes

--- submit_rating.php
+++ submit_rating.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+      text-align: center;
+      margin-top: 100px;
+    }
+
+    .message {
+      font-size: 24px;
+      color: #333;
+      margin-bottom: 20px;
+    }
+
+    .redirect {
+      font-size: 18px;
+      color: #666;
+    }
+  </style>
+</head>
+<body>
 <?php
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -23,11 +47,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $stmt = $conn->prepare ("INSERT INTO `ratings`(`rating`) VALUES (?)");
   $stmt->bind_param("i", $rating);
   $stmt->execute();
-  echo"thank you for your rate";
-  
 
   // Close the statement and database connection
   $stmt->close();
   $conn->close();
+
+  // Display the thank you message and redirect
+  echo '<div class="message">Thank you for your rating!</div>';
+  echo '<div class="redirect">You will be redirected to the homepage shortly...</div>';
+
+  // Redirect to homepage.php after 2 seconds
+  header("refresh:2; url=homepage.php");
+  exit(); // Ensure no further code execution after the redirect
 }
 ?>
+</body>
+</html>
